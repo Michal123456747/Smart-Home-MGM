db627795e44f6d7c1725ae100f88b232cce4d365
Grzegorz Plawinski <mcgregor1406@gmail.com>
2022-07-19 14:59:35 +0200

    Modified header for GPIOArduinoController
    Added basic methods to the GPIOArduinoController class

--- Software/headers/GPIOArduinoController.hpp
+++ Software/headers/GPIOArduinoController.hpp
@@ -1,6 +1,19 @@
 #pragma once
 
+
+
+#include <Arduino.h>
+
 class GPIOArduinoController
 {
-    ;
+
+public:
+    GPIOArduinoController();
+
+    bool reserve_pin(byte, byte);
+    unsigned int read_from_pin(byte);
+    void set_to_pin(byte);
+    void set_to_pin(byte);
+
+    ~GPIOArduinoController();
 };
