f48b7ff9486af8df60cde0b66e02270c09ae0e6d
Grzegorz Plawinski <mcgregor1406@gmail.com>
2022-07-20 15:25:39 +0200

    Added a constuctor to PIN class
    Changed PIN from struct to class, added a construcor and defined some variables

--- Software/headers/GPIOArduinoController.hpp
+++ Software/headers/GPIOArduinoController.hpp
@@ -1,12 +1,20 @@
 #pragma once
 
 
-
 #include <Arduino.h>
 #include <vector>
 
+
 namespace GPIO
 {
+
+#define NUM 0
+#define TYPE 1
+#define STATUS 2
+
+#define PWM 0
+#define DIGITAL 1
+#define ANALOG 2
 
 class GPIOController
 {
@@ -15,24 +23,31 @@ class GPIOController
     virtual void set_to_pin() = 0;
 };
 
-struct PIN
+class PIN
 {
-    byte number;
-    byte type;
-    bool status;
+    byte number_;
+    byte type_;
+    bool status_;
+
+public:
+    PIN(byte, byte, bool);
+
+
+
+    ~PIN() = default;
 };
 
 class GPIOArduinoController: public GPIOController
 {
-    std::vector<PIN> pins_reserved;
+    std::vector<PIN> pins_reserved_;
 public:
-    GPIOArduinoController();
+    GPIOArduinoController() = default;
 
     virtual bool reserve_pin(byte, byte, bool) override;
     virtual unsigned int read_from_pin(byte) override;
     virtual void set_to_pin(byte) override;
 
-    ~GPIOArduinoController();
+    ~GPIOArduinoController() = default;
 };
 
 }
--- Software/src/GPIOArduinoController.cpp
+++ Software/src/GPIOArduinoController.cpp
@@ -1,6 +1,12 @@
 
 #include "/home/greggy/projekty_linux/smart-home-mgm/Software/Software/headers/GPIOArduinoController.hpp" // to change
 
+GPIO::PIN::PIN(byte pin_num, byte type, bool status)
+    : number_(pin_num),
+    type_(type),
+    status_(status)
+    {}
+
 bool GPIO::GPIOArduinoController::reserve_pin(byte pin_num, byte type, bool status)
 {
     if(type < 3)
@@ -12,13 +18,14 @@ bool GPIO::GPIOArduinoController::reserve_pin(byte pin_num, byte type, bool stat
     2 - analog
     */
 
-    for(auto i : pins_reserved)
+    for(auto i : pins_reserved_)
     {
-        if(i.number == pin_num)
+        if(i.number_ == pin_num)
             return false;
     }
 
-    
+    PIN reserved;
+
 
     return true;
 }
