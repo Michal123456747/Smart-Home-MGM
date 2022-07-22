2927b47df8da73f5c3cd626ed7c4537f77f1bb93
Michal Ramus <ramus.michal21@gmail.com>
2022-07-22 16:00:24 +0200

    feat: Create Pin type
    Pin type contains all data about pin configuration

---
+++ Software/src/GPIO/Pin.hpp
@@ -0,0 +1,27 @@
+/**
+ * @file Pin.hpp
+ * @brief Contains all data about pin configuration
+ */
+
+#include <stdint.h>
+
+#include "EGPIOTypes.hpp"
+
+#pragma once
+
+namespace GPIO
+{
+    /**
+     * @brief Type that contains all data about pin configuration
+     */
+    struct Pin
+    {
+        const uint8_t number_;
+        const PinType type_;
+        const PinMode mode_;
+
+        Pin(uint8_t number, PinType type, PinMode mode)
+        : number_(number), type_(type), mode_(mode)
+        {}
+    };
+} // namespace GPIO
