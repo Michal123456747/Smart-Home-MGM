6f079cd831eb159c0d43cef32312593ba30968e6
Michal Ramus <ramus.michal21@gmail.com>
2022-08-04 21:44:03 +0200

    feat: Create FreeRTOS.hpp file that contains all mocks of RTOS functions

---
+++ Software/src/FreeRTOSNative/FreeRTOS.hpp
@@ -0,0 +1,14 @@
+/**
+ * @file FreeRTOS.hpp
+ * @brief Contains all files from FreeRTOSNative directory
+ * When you want to use FreeRTOS on native platform, include only this file
+ */
+
+#pragma once
+
+#include "Task.hpp"
+#include "Queue.hpp"
+#include "Semaphore.hpp"
+
+
+//TODO: Add mocking of eventGroups https://www.freertos.org/FreeRTOS-Event-Groups.html
